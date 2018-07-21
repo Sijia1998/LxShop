174be066537a5cbacbdc870829b9b2942c1d38d1
Sijia1998 <894744332@qq.com>
2018-07-21 09:58:55 +0800

    test

---
+++ lxshop/src/components/NavBread.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="nav-breadcrumb-wrap">
+        <div class="container">
+            <nav class="nav-breadcrumb">
+                <a href="/">Home</a>
+                <slot></slot>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {},
+    props: {},
+    data() {
+        return {
+        }
+    },
+    watch: {},
+    computed: {},
+    methods: {},
+    created() { },
+    mounted() { }
+}
+</script>
