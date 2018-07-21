34225dc4ba992a6e3e420c12084a983a001ffd92
Sijia1998 <894744332@qq.com>
2018-07-21 10:00:25 +0800

    配置

--- lxshop/build/webpack.dev.conf.js
+++ lxshop/build/webpack.dev.conf.js
@@ -9,9 +9,20 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
+const express = require('express')
+const app = express()//请求server
+const router = express.Router();
+const goodsData = require('./../mock/goods.json');
+app.use('/api',router);
+// router.get("/goods",function(req,res,next){
+//   res.json(goodsData);
+// })
+
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
+
+
 
 const devWebpackConfig = merge(baseWebpackConfig, {
   module: {
@@ -42,6 +53,15 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     quiet: true, // necessary for FriendlyErrorsPlugin
     watchOptions: {
       poll: config.dev.poll,
+    },
+    before(app){
+      app.get('/api/goods',(req,res) => {
+        // console.log(goodsData);
+        res.json({
+          errno:0,
+          data: goodsData
+        })
+      })
     }
   },
   plugins: [
